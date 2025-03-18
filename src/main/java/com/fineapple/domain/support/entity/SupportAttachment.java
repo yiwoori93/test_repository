07ef163e50663d4d3dd49053fa2b3548940642f2
yiwoori <yiwoori93@naver.com>
2025-03-18 11:50:10 +0900

    주석

--- src/main/java/com/fineapple/domain/support/entity/SupportAttachment.java
+++ src/main/java/com/fineapple/domain/support/entity/SupportAttachment.java
@@ -14,7 +14,7 @@ public class SupportAttachment {
     private String fileName;
     private Integer fileSize;
     private LocalDateTime createdAt;
-    private Long inquiryId; //1234sdaac
+    private Long inquiryId; //1234
 
     @Builder
     public SupportAttachment(Long attachmentId, String fileUrl, String fileName, Integer fileSize, LocalDateTime createdAt, Long inquiryId) {
